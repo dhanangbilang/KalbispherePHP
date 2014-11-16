4d7dcee3e38bb0fc8fac49371ec6a351544a4257
dhanangbilang <dhanilm1@gmail.com>
2014-11-16 21:38:55 +0700

    Upload File

---
+++ Module/menu.php
@@ -0,0 +1,55 @@
+<div id="outter_menu">
+	<table>
+		<tr>
+			<td>
+				<div class="icon_menu">
+					<img src="../Images/icon_home.png" />
+				</div>
+			</td>
+			<td>
+				<p class="text_menu">Home<p>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div class="icon_menu">
+					<img src="../Images/icon_courseschedule.png" />
+				</div>
+			</td>
+			<td>
+				<p class="text_menu">Home<p>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div class="icon_menu">
+					<img src="../Images/icon_exam.png" />
+				</div>
+			</td>
+			<td>
+				<p class="text_menu">Home<p>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div class="icon_menu">
+					<img src="../Images/icon_attendance.png" />
+				</div>
+			</td>
+			<td>
+				<p class="text_menu">Home<p>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div class="icon_menu">
+					<img src="../Images/icon_score.png" />
+				</div>
+			</td>
+			<td>
+				<p class="text_menu">Home<p>
+			</td>
+		</tr>
+		
+	</table>
+</div>
